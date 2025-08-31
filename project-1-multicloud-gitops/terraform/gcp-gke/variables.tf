1563d79700b41a337b581899356ae1a8e1a037e6
Chad Lester <chad-filevine@chadlester.com>
2025-08-31 16:07:05 -0700

    multi-cloud: add GKE Terraform + GitOps manifests; docs evidence

--- project-1-multicloud-gitops/terraform/gcp-gke/variables.tf
+++ project-1-multicloud-gitops/terraform/gcp-gke/variables.tf
@@ -1,5 +1,23 @@
-variable "project"      { type = string }
-variable "region"       { type = string  default = "us-central1" }
-variable "cluster_name" { type = string  default = "demo-gke" }
-variable "node_count"   { type = number  default = 2 }
-variable "machine_type" { type = string  default = "e2-standard-2" }
+variable "project" {
+  type = string
+}
+
+variable "region" {
+  type    = string
+  default = "us-central1"
+}
+
+variable "cluster_name" {
+  type    = string
+  default = "demo-gke"
+}
+
+variable "node_count" {
+  type    = number
+  default = 2
+}
+
+variable "machine_type" {
+  type    = string
+  default = "e2-standard-2"
+}
--- project-1-multicloud-gitops/terraform/gcp-gke/versions.tf
+++ project-1-multicloud-gitops/terraform/gcp-gke/versions.tf
@@ -1,7 +1,14 @@
 terraform {
   required_version = ">= 1.6.0"
   required_providers {
-    google = { source = "hashicorp/google", version = ">= 5.0" }
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 5.0"
+    }
   }
 }
-provider "google" { project = var.project, region = var.region }
+
+provider "google" {
+  project = var.project
+  region  = var.region
+}
