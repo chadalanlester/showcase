c96411976fcd68fceb28b68ffabfdefaaed2597f
Chad Lester <chad.lester@outlook.com>
2025-09-04 09:54:59 -0700

    fix(p6-aks-gitops): convert one-line HCL blocks to valid multi-line in variables.tf and main.tf

--- project-6-azure-aks-gitops/terraform/aks/variables.tf
+++ project-6-azure-aks-gitops/terraform/aks/variables.tf
@@ -1,23 +1,70 @@
-variable "subscription_id"      { type = string }
-variable "location"             { type = string  default = "eastus2" }
-variable "name_prefix"          { type = string  default = "p6" }
-variable "resource_group_name"  { type = string  default = "rg-aks-gitops-lab" }
-variable "tags" {
-  type = map(string)
-  default = { project = "project-6-azure-aks-gitops", owner = "chad", env = "lab" }
+variable "subscription_id" { type = string }
+
+variable "location" {
+  type    = string
+  default = "eastus2"
 }
 
-# AKS
-variable "kubernetes_version" { type = string  default = null } # let AKS choose latest if null
-variable "system_vm_size"     { type = string  default = "Standard_D4s_v5" }
-variable "user_vm_size"       { type = string  default = "Standard_D4s_v5" }
-variable "user_min"           { type = number  default = 1 }
-variable "user_max"           { type = number  default = 3 }
+variable "name_prefix" {
+  type    = string
+  default = "p6"
+}
 
-# GitOps
-variable "gitops_repo_url"    { type = string  default = "https://github.com/chadalanlester/showcase.git" }
-variable "gitops_repo_branch" { type = string  default = "main" }
-variable "gitops_repo_path"   { type = string  default = "project-6-azure-aks-gitops/gitops/clusters/lab" }
+variable "resource_group_name" {
+  type    = string
+  default = "rg-aks-gitops-lab"
+}
 
-# Policy (optional initiative id). Leave empty to skip assignment.
-variable "policy_set_definition_id" { type = string  default = "" }
+variable "tags" {
+  type = map(string)
+  default = {
+    project = "project-6-azure-aks-gitops"
+    owner   = "chad"
+    env     = "lab"
+  }
+}
+
+variable "kubernetes_version" {
+  type    = string
+  default = null
+}
+
+variable "system_vm_size" {
+  type    = string
+  default = "Standard_D4s_v5"
+}
+
+variable "user_vm_size" {
+  type    = string
+  default = "Standard_D4s_v5"
+}
+
+variable "user_min" {
+  type    = number
+  default = 1
+}
+
+variable "user_max" {
+  type    = number
+  default = 3
+}
+
+variable "gitops_repo_url" {
+  type    = string
+  default = "https://github.com/chadalanlester/showcase.git"
+}
+
+variable "gitops_repo_branch" {
+  type    = string
+  default = "main"
+}
+
+variable "gitops_repo_path" {
+  type    = string
+  default = "project-6-azure-aks-gitops/gitops/clusters/lab"
+}
+
+variable "policy_set_definition_id" {
+  type    = string
+  default = ""
+}
