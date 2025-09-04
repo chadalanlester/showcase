8cc3390ec43fc7351714a0975102c05e2427fb73
Chad Lester <chad.lester@outlook.com>
2025-09-04 11:05:54 -0700

    fix(p6-aks-gitops): repair AKS block and set upgrade_settings max_unavailable=1

--- project-6-azure-aks-gitops/terraform/aks/main.tf
+++ project-6-azure-aks-gitops/terraform/aks/main.tf
@@ -25,6 +25,7 @@ data "azurerm_key_vault" "kv" {
 }
 
 resource "azurerm_kubernetes_cluster" "aks" {
+resource "azurerm_kubernetes_cluster" "aks" {
   name                = local.name
   location            = var.location
   resource_group_name = data.azurerm_resource_group.rg.name
@@ -40,15 +41,16 @@ resource "azurerm_kubernetes_cluster" "aks" {
 
   default_node_pool {
     name                         = "system"
-    vm_size                      = var.system_vm_size # set in variables.tf (use Standard_B1ms/B2s)
+    vm_size                      = var.system_vm_size
     type                         = "VirtualMachineScaleSets"
     node_count                   = 1
     only_critical_addons_enabled = true
     orchestrator_version         = var.kubernetes_version
     upgrade_settings {
       max_surge        = "0"
-      max_unavailable = "1"
+      max_unavailable  = "1"
     }
+  }
 
   network_profile {
     network_plugin    = "azure"
@@ -56,10 +58,8 @@ resource "azurerm_kubernetes_cluster" "aks" {
     outbound_type     = "loadBalancer"
   }
 
-  # AAD managed RBAC (v3 warns; safe, required true)
   azure_active_directory_role_based_access_control { managed = true }
 
-  # Use existing LAW for control-plane / daemonset agent
   oms_agent {
     log_analytics_workspace_id = data.azurerm_log_analytics_workspace.law.id
   }
@@ -68,69 +68,4 @@ resource "azurerm_kubernetes_cluster" "aks" {
   azure_policy_enabled = true
 
   tags = var.tags
-}
-
-resource "azurerm_kubernetes_cluster_node_pool" "user" {
-  name                  = "workload"
-  kubernetes_cluster_id = azurerm_kubernetes_cluster.aks.id
-  vm_size               = var.user_vm_size
-  node_count            = var.user_min
-  min_count             = var.user_min
-  max_count             = var.user_max
-  enable_auto_scaling   = true
-  mode                  = "User"
-  orchestrator_version  = var.kubernetes_version
-  max_pods              = 60
-  node_taints           = []
-  tags                  = var.tags
-}
-
-# Allow AKS to pull images from existing ACR
-resource "azurerm_role_assignment" "acrpull" {
-  scope                = data.azurerm_container_registry.acr.id
-  role_definition_name = "AcrPull"
-  principal_id         = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
-}
-
-# Flux extension + config from this repo
-resource "azurerm_kubernetes_cluster_extension" "flux" {
-  name           = "flux"
-  cluster_id     = azurerm_kubernetes_cluster.aks.id
-  extension_type = "flux"
-  release_train  = "Stable"
-}
-
-resource "azurerm_kubernetes_flux_configuration" "gitops" {
-  name       = "cluster-gitops"
-  cluster_id = azurerm_kubernetes_cluster.aks.id
-  scope      = "cluster"
-  namespace  = "flux-system"
-
-  git_repository {
-    url                      = var.gitops_repo_url
-    reference_type           = "branch"
-    reference_value          = var.gitops_repo_branch
-    sync_interval_in_seconds = 60
-    timeout_in_seconds       = 600
-  }
-
-  kustomizations {
-    name                       = "apps"
-    path                       = var.gitops_repo_path
-    garbage_collection_enabled = true
-    retry_interval_in_seconds  = 30
-    sync_interval_in_seconds   = 60
-    timeout_in_seconds         = 600
-  }
-
-  depends_on = [azurerm_kubernetes_cluster_extension.flux]
-}
-
-# Optional: assign an initiative at the AKS resource if provided
-resource "azurerm_resource_policy_assignment" "aks_initiative" {
-  count                = var.policy_set_definition_id != "" ? 1 : 0
-  name                 = "${var.name_prefix}-aks-security"
-  policy_definition_id = var.policy_set_definition_id
-  resource_id          = azurerm_kubernetes_cluster.aks.id
-  location             = var.location
 }
